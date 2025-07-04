453be85e6e75b08e02cb5d9516aa15331e8054fa
Unknownuser0011 <andrew1112111@gmail.com>
2025-07-04 22:44:55 +0800

    VERSION 0.5 got lazy

--- shipprogram.java
+++ shipprogram.java
@@ -1,6 +1,14 @@
 
+import java.util.Scanner;
 
-class playerturn{
+
+
+class playerturn(board){
+
+    Scanner get_playerturn = new Scanner(System.in);
+
+
+
 
 }
 
@@ -14,11 +22,18 @@ public class shipprogram {
 
     public static void main(String[] args){
 
-    System.out.println("");        
+    int choice = 0;
+
+    System.out.println("WELCOME TO BATTLESHIP!");
+    System.err.println("Press 1 to play \n Press 2 to Quit");
+
+
+    if(choice == 1){
+
+    } else if(choice == 2){
+        System.exit(choice);
+    }
     
 
-
-
-    }
-
-}
+    }//main
+}//class
