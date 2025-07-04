0f32c327579ba59f9ecdc6568345116a5c9bde02
Unknownuser0011 <andrew1112111@gmail.com>
2025-07-04 22:36:14 +0800

    FIRST VERSION

---
+++ shipprogram.java
@@ -0,0 +1,24 @@
+
+
+class playerturn{
+
+}
+
+class board {
+
+    boolean[][] board = new boolean[5][5]; 
+}
+
+
+public class shipprogram {
+
+    public static void main(String[] args){
+
+    System.out.println("");        
+    
+
+
+
+    }
+
+}
