f98dcf86357bb9a2dc3106d6b53cffa9c9765ad1
Unknownuser0011 <andrew1112111@gmail.com>
2025-07-16 15:43:12 +0800

    REVISED VERSION 3.5

--- Main.java
+++ Main.java
@@ -7,7 +7,7 @@ public class Main {
 
         Board Board_ship = new Board();
         Player Player1 = new Player();
-        Player player2 = new Player();
+        Player Player2 = new Player();
 
         try (Scanner Getinput = new Scanner(System.in)) {
             boolean GameOver = false;
@@ -16,18 +16,34 @@ public class Main {
                 int input_row = 0;
                 int input_column = 0;
                 
-                boolean Player_turn_over = false;
-                while(!Player_turn_over){
-                    
+                boolean Player1_turn_over = false;
+                boolean Player2_turn_over = false;
+                //player 1 turn 
+                while(!Player1_turn_over){
+                    System.out.println("++=PLAYER 1 TURN=++");
                     input_row = Player1.Player_Turn_Row(Getinput);
                     input_column = Player1.Player_Turn_Column(Getinput);
                     
-                    if (Board_ship.CheckTurn(input_row, input_column) == true){
+                    if (Board_ship.CheckTurn_Player1(input_row, input_column) == true){
                         Board_ship.PrintBoard();
-                        Player_turn_over = false;
+                        Player1_turn_over = false;
                     } else {
                         Board_ship.PrintBoard();
-                        Player_turn_over = true;
+                        Player1_turn_over = true;
+                    }
+                }
+                //Player 2 turn 
+                while(!Player2_turn_over){
+                    System.out.println("++=PLAYER 1 TURN=++");
+                    input_row = Player2.Player_Turn_Row(Getinput);
+                    input_column = Player2.Player_Turn_Column(Getinput);
+                    
+                    if (Board_ship.CheckTurn_Player2(input_row, input_column) == true){
+                        Board_ship.PrintBoard();
+                        Player2_turn_over = false;
+                    } else {
+                        Board_ship.PrintBoard();
+                        Player2_turn_over = true;
                     }
                 }
                 
--- board.java
+++ board.java
@@ -11,19 +11,41 @@ int[][] Place_Board = {
     {0, 1, 0, 0, 0, 0, 0}, // row 6 coloumn 6     
 };
 
+
+//PLAYER 1 is 1
+//PLAYER 2 is 2
+//HITTED SHIPS FOR PLAYER 1 is 3
+//HITTED SHIPS FOR PLAYER 2 is 4
+
+
 //column is VERTICAL
 //row is Horizontal
-// Therefore Place_Board[row][column]
+// So Place_Board[row][column]
 
     //checking if player place input hit or miss a ship
-    boolean CheckTurn(int input_row, int input_column){
+    boolean CheckTurn_Player1(int input_row, int input_column){
         boolean CheckifHit = false;
 
         if (Place_Board[input_row][input_column] != 0){
             System.out.println("HIT");
-            Place_Board[input_row][input_column] = 2;
+            //4 is when player 1 hiited a ship from player 2
+            Place_Board[input_row][input_column] = 4;
             CheckifHit = true;
-            //2 is when the player hit a ship
+        } else{
+            System.out.println("MISS");
+            CheckifHit = false;
+        }        
+        return CheckifHit;
+    }
+
+    boolean CheckTurn_Player2(int input_row, int input_column){
+        boolean CheckifHit = false;
+
+        if (Place_Board[input_row][input_column] != 0){
+            System.out.println("HIT");
+            //3 is when player 1 hiited a ship from player 2
+            Place_Board[input_row][input_column] = 3;
+            CheckifHit = true;
         } else{
             System.out.println("MISS");
             CheckifHit = false;
@@ -31,13 +53,12 @@ int[][] Place_Board = {
         return CheckifHit;
     }
     
-
     boolean CheckBoard(){
         //checking if there is a ship on the board
         int ShipRemaining = 0;
         for (int i = 0; i < Place_Board.length; i++){
             for (int y = 0; y < Place_Board[i].length; y++){
-                 if (Place_Board[i][y] == 1){
+                 if (Place_Board[i][y] == 1 || Place_Board[i][y] == 2){
                     ShipRemaining++;  
                 } 
             }
@@ -59,10 +80,17 @@ int[][] Place_Board = {
 
             // Print the vertical lines
             for (int column = 0; column < Place_Board[row].length; column++) {
-                if (Place_Board[row][column] == 2){
-                    System.out.print("| X ");   
-                }else {
-                    System.out.print("|   ");
+        
+                switch (Place_Board[row][column]) {
+                    case 3:
+                        System.out.print("| X ");
+                        break;
+                    case 4:
+                        System.out.print("| Y ");
+                        break;
+                    default:
+                        System.out.print("|   ");
+                        break;
                 }
             }
             System.out.println("|");
