264f166ef3312bf2ff0f4809b8e1b113aa4887cf
Unknownuser0011 <andrew1112111@gmail.com>
2025-09-14 09:49:24 +0800

    AI

--- Main.java
+++ Main.java
@@ -10,39 +10,38 @@ public class Main {
         Player Player2 = new Player();
         AI AI_Player = new AI();
         
-        /*
-         * System.out.println("========================================");
-        System.out.println("WELCOME TO BATTLESHIP!");
-        System.out.println("select mode\n 1. Player vs Player \n 2. Player vs AI \n 3. Exit");
-        System.out.println("Enter your options: ");
-         * 
-         */
-        
-
+      
+        //Scanner security or something 
         try (Scanner Getinput = new Scanner(System.in)) {
            
-            int shipCount = 0;
-            int maxShips = 5;
+            //max ships and ship count 
+            int Ship_Count = 0;
+            int Max_ships = 5;
+            
             Player1.Name = "and";
             Player2.Name = "rew";
 
             Player1.OP_Name = Player2.Name;
             Player2.OP_Name = Player1.Name;
             //placing ships 
-            Player1.PLACING_PLAYER_SHIPS(shipCount, maxShips, Getinput);
-            Player2.PLACING_PLAYER_SHIPS(shipCount, maxShips, Getinput);
+            Player1.PLACING_PLAYER_SHIPS(Ship_Count, Max_ships, Getinput);
+            Player2.PLACING_PLAYER_SHIPS(Ship_Count, Max_ships, Getinput);
        
             Boolean GameOver = false;
+
+            //Game start PLayer vs Player 
             while(!GameOver){
                 //Player 1 turn
                 Player2.PLAYER_TURN(Getinput);
                 //Player 2 turn 
-                Player1.PLAYER_TURN(Getinput);
                 
+                //checking board    
                 if (Player1.Checking_the_board() == true){
-                    System.out.println("Player 1 lost ");
+                    System.out.println("Player " + Player1.Name + " lost ");
+                    GameOver = true;
                 } else if (Player2.Checking_the_board() == true){
-                    System.out.println("Player 2 lost ");
+                    System.out.println("Player " + Player2.Name + " lost ");
+                    GameOver = true;
                 }
 
            }//while loop
